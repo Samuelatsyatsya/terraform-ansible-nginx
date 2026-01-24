c25c9b4cbfb4f11e8db2c4fc4c512622526578f9
Samuel Atsyatsya <atsyatsya1@gmail.com>
2026-01-24 23:43:42 +0000

    feat: add compute(ec2)

---
+++ terraform/modules/compute/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "this" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = var.security_group_ids
+  key_name                    = aws_key_pair.this.key_name
+  associate_public_ip_address = false
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project}-app"
+    }
+  )
+}
