f9c11bf6d220a96223155e805689b109e9ec320b
Samuel Atsyatsya <atsyatsya1@gmail.com>
2026-01-28 23:03:19 +0000

    refactor(terraform): remove dynamo table resource creation and it outputs after creation

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -27,19 +27,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
   }
 }
 
-
-#State Locking Table
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-ansible-nginx-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Project = "terraform-ansible-nginx"
-  }
-}
